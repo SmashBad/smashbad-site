483672578628b3c07943e76441871c8d596dc287
SmashBad <smashbad.contact@gmail.com>
2025-08-23 15:13:41 +0200

    maj favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,16 @@
 // pages/_app.tsx
 import type { AppProps } from "next/app";
-import "../styles/global.css"; // <-- ton ancien styles.css déplacé ici
 import Head from "next/head";
+import "../styles/global.css";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
-
-export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        {/* pick the ones you added in /public */}
+        {/* Favicon(s) – place les fichiers dans /public */}
         <link rel="icon" href="/favicon.ico" />
         <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         <meta name="theme-color" content="#010B2E" />
       </Head>
       <Component {...pageProps} />
