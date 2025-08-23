aba09ec459c7278f8fd9f0289318228d32bd3450
SmashBad <smashbad.contact@gmail.com>
2025-08-23 11:54:35 +0200

    ajout favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,23 @@
 // pages/_app.tsx
 import type { AppProps } from "next/app";
 import "../styles/global.css"; // <-- ton ancien styles.css déplacé ici
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
+}
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        {/* pick the ones you added in /public */}
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <meta name="theme-color" content="#010B2E" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
