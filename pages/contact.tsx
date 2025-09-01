7c0407ed46dede0aa08e020d5d3665de69f8cc2c
SmashBad <smashbad.contact@gmail.com>
2025-09-01 18:09:59 +0200

    formulaire intégré

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -4,7 +4,13 @@ import Head from "next/head";
 export default function ContactPage() {
   return (
     <>
-        <iframe className="airtable-embed" src="https://airtable.com/embed/appErxcur9vTL9X8t/paggdZiHNRGqOeORz/form" onmousewheel="" width="60%" height="700" style="background: #040E2E; border: 1px solid #12D8DF;"></iframe>
+        <iframe
+            className="airtable-embed"
+            src="https://airtable.com/embed/appErxcur9vTL9X8t/paggdZiHNRGqOeORz/form"
+            width="60%"
+            height={700}
+            style={{ background: "#040E2E", border: "1px solid #12D8DF" }}
+        />
     </>
   );
 }
