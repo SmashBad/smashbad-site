9c4fe28d6534b6f69d7d69e59f91b6f65e261ab2
SmashBad <smashbad.contact@gmail.com>
2025-09-03 19:21:50 +0200

    update footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,16 +5,17 @@ import Link from "next/link";
 export default function Footer() {
   return (
     <footer className="footer" role="contentinfo">
-      <small>
         <Link
           href="/contact"
           className="underline underline-offset-4 hover:no-underline"
           aria-label="Formulaire de contact"
         >
-        Contact
-        </Link><br />
-        © {new Date().getFullYear()} SMASH.bad — Tous droits réservés
-      </small>
+          Contact
+        </Link>
+        <br />
+        <small>
+         © {new Date().getFullYear()} SMASH.bad — Tous droits réservés
+        </small>
     </footer>
   );
 }
