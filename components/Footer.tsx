cdec105fe9cd7ca1bb1420bee0e696a4dffc4d39
SmashBad <smashbad.contact@gmail.com>
2025-09-03 19:34:18 +0200

    inversion droits et contact

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,15 +5,14 @@ import Link from "next/link";
 export default function Footer() {
   return (
     <footer className="footer" role="contentinfo">
-        <Link
+                 © {new Date().getFullYear()} SMASH.bad — Tous droits réservés<br />
+         <Link
           href="/contact"
           className="underline underline-offset-4 hover:no-underline"
           aria-label="Formulaire de contact"
         >
           Contact
         </Link>
-         <br />
-         © {new Date().getFullYear()} SMASH.bad — Tous droits réservés
     </footer>
   );
 }
